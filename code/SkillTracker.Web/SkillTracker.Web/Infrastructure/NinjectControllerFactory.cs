24ca5165a3a5d063163ad07f98a10b21ca9eb900
Sergey <smar.personal@gmail.com>
2013-09-29 20:06:26 +0300

    Ninject factory has been added.

---
+++ code/SkillTracker.Web/SkillTracker.Web/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SkillTracker.Web.Infrastructure
+{
+  /// <summary>
+  /// Ninject controller factory.
+  /// </summary>
+  public class NinjectControllerFactory : DefaultControllerFactory
+  {
+    /// <summary>
+    /// The ninject kernel
+    /// </summary>
+    private IKernel ninjectKernel;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NinjectControllerFactory"/> class.
+    /// </summary>
+    public NinjectControllerFactory()
+    {
+      ninjectKernel = new StandardKernel();
+      AddBindings();
+    }
+
+    /// <summary>
+    /// Retrieves the controller instance for the specified request context and controller type.
+    /// </summary>
+    /// <param name="requestContext">The context of the HTTP request, which includes the HTTP context and route data.</param>
+    /// <param name="controllerType">The type of the controller.</param>
+    /// <returns>
+    /// The controller instance.
+    /// </returns>
+    protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+    {
+      if (controllerType == null)
+      {
+        return null;
+      }
+
+      return (IController) ninjectKernel.Get(controllerType);
+    }
+
+    /// <summary>
+    /// Adds the IoC bindings.
+    /// </summary>
+    protected virtual void AddBindings()
+    {
+
+    }
+  }
+}

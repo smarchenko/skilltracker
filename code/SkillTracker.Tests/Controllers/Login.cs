0715992d077c7749c51f3d80d2872c0d07aef147
smarchenko <smar.personal@gmail.com>
2014-01-07 16:53:07 +0200

    Login View stub has been added.

---
+++ code/SkillTracker.Tests/Controllers/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkillTracker.Web.Models;
+
+namespace SkillTracker.Tests.Controllers
+{
+  [TestClass]
+  public class Login
+  {
+    [TestMethod]
+    public void IndexAction_ReturnView_Tester()
+    {
+      var controller = new LoginController();
+      var result = controller.Index();
+
+      Assert.IsTrue(result is ViewResult);
+      Assert.AreEqual((result as ViewResult).ViewName, "Login");
+    }
+  }
+}

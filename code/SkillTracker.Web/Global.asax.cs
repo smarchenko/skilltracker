bc877846564faf4cc46a703bc6dc17e81c37b1ea
smarchenko <smar.personal@gmail.com>
2014-01-17 08:57:17 +0200

    Header has been added to a design

--- code/SkillTracker.Web/Global.asax.cs
+++ code/SkillTracker.Web/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using SkillTracker.Web.Infrastructure;
 
@@ -16,7 +17,7 @@ namespace SkillTracker.Web
       WebApiConfig.Register(GlobalConfiguration.Configuration);
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
-
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
       ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
     }
   }

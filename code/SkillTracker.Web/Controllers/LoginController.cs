a6a88eaa810e58f6b4f1790eead7e4c8c1d1962f
smarchenko <smar.personal@gmail.com>
2014-01-18 19:57:06 +0200

    Footer and middle part of the Login form has been added

---
+++ code/SkillTracker.Web/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkillTracker.Web.Models
+{
+	public class LoginController : Controller
+	{
+		//        
+		// GET: /Login/
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View("Login");
+		}
+
+		[HttpPost]
+		public ActionResult Index(string usermail, string password)
+		{
+			return View("Login");
+		}
+
+	}
+}

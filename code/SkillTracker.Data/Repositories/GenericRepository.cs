421809bff2358443ad864a975c080d9a49b7c7c2
smarchenko <smar.personal@gmail.com>
2014-02-27 21:43:22 +0200

    Basic repository functionality has been added.

---
+++ code/SkillTracker.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SkillTracker.Data.Repositories
+{
+  public class GenericRepository<TEntity> where TEntity : class
+  {
+    private readonly SkillTrackerContext _context;
+
+    private readonly DbSet<TEntity> _dbSet;
+
+    public GenericRepository(SkillTrackerContext context)
+    {
+      Contract.Requires<ArgumentNullException>(context != null, "context");
+
+      _context = context;
+      _dbSet = context.Set<TEntity>();
+    }
+
+    public virtual IEnumerable<TEntity> Get(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        string includeProperties = "")
+    {
+      Contract.Requires<ArgumentNullException>(includeProperties != null);
+      IQueryable<TEntity> query = _dbSet;
+
+      if (filter != null)
+      {
+        query = query.Where(filter);
+      }
+
+      foreach (var includeProperty in includeProperties.Split
+          (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+      {
+        query = query.Include(includeProperty);
+      }
+
+      if (orderBy != null)
+      {
+        return orderBy(query).ToList();
+      }
+      else
+      {
+        return query.ToList();
+      }
+    }
+
+    public virtual TEntity GetById(object id)
+    {
+      return _dbSet.Find(id);
+    }
+
+    public virtual void Insert(TEntity entity)
+    {
+      _dbSet.Add(entity);
+    }
+
+    public virtual void Delete(object id)
+    {
+      TEntity entityToDelete = _dbSet.Find(id);
+      Delete(entityToDelete);
+    }
+
+    public virtual void Delete(TEntity entityToDelete)
+    {
+      if (_context.Entry(entityToDelete).State == EntityState.Detached)
+      {
+        _dbSet.Attach(entityToDelete);
+      }
+      _dbSet.Remove(entityToDelete);
+    }
+
+    public virtual void Update(TEntity entityToUpdate)
+    {
+      _dbSet.Attach(entityToUpdate);
+      _context.Entry(entityToUpdate).State = EntityState.Modified;
+    }
+  }
+}
---
+++ code/SkillTracker.Data/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using SkillTracker.Data.Repositories;
+
+namespace SkillTracker.Data
+{
+  public class UnitOfWork : IDisposable
+  {
+    private readonly SkillTrackerContext _context = new SkillTrackerContext();
+    private GenericRepository<Department> _departmentRepository;
+    private bool _disposed = false;
+
+    public UnitOfWork(SkillTrackerContext context)
+    {
+      Contract.Requires<ArgumentNullException>(context != null, "context");
+      this._context = context;
+    }
+
+    public virtual GenericRepository<Department> DepartmentRepository
+    {
+      get
+      {
+        if (_departmentRepository == null)
+        {
+          _departmentRepository = new GenericRepository<Department>(_context);
+        }
+
+        return _departmentRepository;
+      }
+    }
+
+    public void Save()
+    {
+      _context.SaveChanges();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (!this._disposed)
+      {
+        if (disposing)
+        {
+          _context.Dispose();
+        }
+      }
+      this._disposed = true;
+    }
+
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+  }
+}
